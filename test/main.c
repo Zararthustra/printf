148a2aab415a4ee1e30498f8f24adea8e7dc8b3f
TS-N <2501@holbertonschool.com>
2020-10-27 09:58:49 +0000

    move main to test/

--- test/main.c
+++ test/main.c
@@ -1,13 +1,8 @@
-#include "./holberton.h"
-#include <limits.h>
-#include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
+#include "holberton.h"
+
+int	main()
 {
-	_printf("classic %c %d %d %i %i %i %s", 'V', -3, 55, -2147483648, 2147483647, 0, "Vendetta");
+	int	nb = 0;
+	printf("%llu\n", &nb);
 	return (0);
 }
